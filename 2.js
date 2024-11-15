800637c82457c7486b5c08046c01c7d7c1fd6a7f
Rahim Haq <rahimtahajjadanzahirhaq@gmail.com>
2024-11-15 16:36:00 +0700

    Fix page

--- 2.js
+++ 2.js
@@ -3,7 +3,6 @@ const sebelum = ["u", "D", "m", "w", "b", "a", "y", "s", "i", "s", "w", "a", "e"
 function sortArray(sebelum) {
     const sesudah = ["D", "u", "m", "b", "w", "a", "y", "s", " ", "i", "s", " ", "a", "w", "e", "s", "o", "m", "e"];
     
-    // Menggabungkan array menjadi string dengan menggunakan
     return sesudah.join("");
   }
 
--- 3.js
+++ 3.js
@@ -2,13 +2,9 @@ function cetakPola(n) {
     let totalKarakter = n * 2 - 1; // Total karakter di baris pertama (paling atas)
   
     for (let i = 0; i < n; i++) {
-      // Menambahkan spasi di awal setiap baris untuk membuat segitiga sama kaki
       let spasi = " ".repeat(i);
-      
-      // Membuat pola karakter `#` dan `+` untuk setiap baris
       let baris = "";
       for (let j = 0; j < totalKarakter - (i * 2); j++) {
-        // Untuk baris pertama dan baris ke 2, tambahkan # dan + bergantian
         if (i === 0) {
           baris += (j % 2 === 0 ? "#" : "+");
         } else if (i % 2 === 0) {
@@ -18,11 +14,9 @@ function cetakPola(n) {
         }
       }
   
-      // Cetak baris dengan spasi awal
       console.log(spasi + baris);
     }
   }
   
-  // Contoh penggunaan fungsi
   cetakPola(5);
   
